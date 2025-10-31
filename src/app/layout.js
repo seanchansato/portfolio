38c4e23eddcdf8afd49a1cdc2c5a202b20bca5bf
seanchansato <seanchansato@gmail.com>
2025-10-31 13:32:49 -0400

    New Logo Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,20 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Sean Chan-Sato',
   description: 'Personal website of Sean Chan-Sato',
+  icons: {
+    icon: [
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '16x16' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '32x32' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '48x48' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '96x96' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '192x192' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '256x256' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '384x384' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '512x512' },
+      { url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: 'any' },
+    ],
+    apple: [{ url: '/IMG_3826.PNG?v=2', type: 'image/png', sizes: '180x180' }],
+  },
 }
 
 export default function RootLayout({ children }) {
